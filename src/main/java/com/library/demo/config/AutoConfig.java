2b716f15d908bc65a235a96f564db2e70ee0101e
yang <ywj9811@gmail.com>
2024-05-14 03:14:26 +0900

    feat : customFilter 구현

---
+++ src/main/java/com/library/demo/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.library.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.library")
+public class AutoConfig {
+}
---
+++ src/main/java/com/library/demo/core/CustomFilter.java
@@ -0,0 +1,20 @@
+package com.library.demo.core;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("this is customFilter !!");
+    }
+}
